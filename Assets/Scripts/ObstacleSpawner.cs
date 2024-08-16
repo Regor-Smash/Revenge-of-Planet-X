42f7d2ba71b5d5f111b408d11a4e9fb9c2924df1
Roger <roger@dalkefamily.com>
2024-08-16 16:50:51 -0700

    Updated unity to v6 preview

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obstacle;
+
+    private bool spawning = true;
+    private Bounds spawnBounds;
+    
+    private void Start()
+    {
+        spawnBounds = GetComponent<Collider2D>().bounds;
+        StartCoroutine(SpawningTimer());
+    }
+
+    private IEnumerator SpawningTimer()
+    {
+        while (spawning)
+        {
+            SpawnObstacle();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        //Spawns an obstacle somewhere on the bound's vertical center line
+        float spawnX = spawnBounds.center.x;//Random.Range(spawnBounds.min.x, spawnBounds.max.x);
+        float spawnY = Random.Range(spawnBounds.min.y, spawnBounds.max.y);
+        Vector3 spawnPos = new Vector3(spawnX, spawnY);
+        Instantiate<GameObject>(obstacle, spawnPos, Quaternion.identity, transform);
+    }
+}
